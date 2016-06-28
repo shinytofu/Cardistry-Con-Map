5594d405de10f84cf0ceef4d415a18bcb97bba3e
Desuvader <desuvader@gmail.com>
2016-06-28 19:56:10 +0200

    Add about text

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 window.onload = function() {
+  /*
   if (document.querySelector('#page')) {
     document.querySelector('#page').innerHTML = '<div id="app"><div id="map"></div><list id="list"></list></div>';
-  }
+  }*/
   var mapStyle = [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}];
   var map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 45, lng: 18 },
@@ -28,7 +29,7 @@ window.onload = function() {
           lat: parseFloat(_.get(entry, 'gsx$lat.$t')),
           lng: parseFloat(_.get(entry, 'gsx$lng.$t')),
           countryCode: countryCode,
-          flagUrl: countryCode === 'catalonia' ? 'http://i.imgur.com/DUpGoKm.png' : 'https://lipis.github.io/flag-icon-css/flags/4x3/' + countryCode + '.svg' 
+          flagUrl: countryCode === 'catalonia' ? 'http://i.imgur.com/DUpGoKm.png' : 'https://lipis.github.io/flag-icon-css/flags/4x3/' + countryCode + '.svg'
         };
         return metaData;
       });
